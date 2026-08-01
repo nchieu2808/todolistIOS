1023cc7d11da6266e0f8ea6d96488f3d18a3d14d
nchieu2808 <nch2808@gmail.com>
2026-08-01 15:01:01 +0700

    Initial Commit

---
+++ TodoListIOSTests/TodoListIOSTests.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListIOSTests.swift
+//  TodoListIOSTests
+//
+//  Created by Nguyễn Chí Hiếu on 1/8/26.
+//
+
+import Testing
+
+struct TodoListIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ TodoListIOSUITests/TodoListIOSUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  TodoListIOSUITestsLaunchTests.swift
+//  TodoListIOSUITests
+//
+//  Created by Nguyễn Chí Hiếu on 1/8/26.
+//
+
+import XCTest
+
+final class TodoListIOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
